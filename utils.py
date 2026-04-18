1cbbe078d44c5416dfbcc4d7801be6693411cff3
royabhrajyoti961-png <royabhrajyoti961@gmail.com>
2026-04-18 13:44:05 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,17 +1,2 @@
-import pyttsx3
-
-def style_risk(status):
-    if status == "High Risk":
-        return f"<span style='color:red;font-weight:bold'>{status}</span>"
-    elif status == "Medium Risk":
-        return f"<span style='color:orange'>{status}</span>"
-    else:
-        return f"<span style='color:green'>{status}</span>"
-
-def speak_alert(text):
-    try:
-        engine = pyttsx3.init()
-        engine.say(text)
-        engine.runAndWait()
-    except:
-        pass
+def dummy():
+    return True
