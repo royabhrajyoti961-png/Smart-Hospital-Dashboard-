19fc9b583bf5c831514980c6ba4c14237b1a4df1
royabhrajyoti961-png <royabhrajyoti961@gmail.com>
2026-04-18 13:43:35 +0530

    Update ai_model.py

--- ai_model.py
+++ ai_model.py
@@ -1,19 +1,7 @@
 def predict_risk(hr, oxygen):
-    score = 0
-
-    if oxygen < 90:
-        score += 2
-    elif oxygen < 95:
-        score += 1
-
-    if hr > 120 or hr < 50:
-        score += 2
-    elif hr > 100:
-        score += 1
-
-    if score >= 3:
+    if oxygen < 90 or hr > 120:
         return "High Risk"
-    elif score == 2:
+    elif oxygen < 95:
         return "Medium Risk"
     else:
         return "Low Risk"
